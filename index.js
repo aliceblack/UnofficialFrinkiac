15daea1ce06e7ce51b6001d3235a4de1ac1d8587
Alice Sasso <alice.sasso@wintech.it>
2021-01-21 11:48:09 +0100

    PORT

--- index.js
+++ index.js
@@ -15,17 +15,17 @@ app.get('/', (req, res) => {
 
 
 app.get('/bot', (req, res) => {
-    let botToken=process.env.BOT_TOKEN;
-    let website="https://api.telegram.org/bot".concat(botToken);
+    var botToken=process.env.BOT_TOKEN;
+    var website="https://api.telegram.org/bot".concat(botToken);
 
-    let chatId=req.body["message"]["chat"]["id"];
-    let name=req.body["message"]["chat"]["first_name"];
-    let text=req.body["message"]["text"];
+    var chatId=req.body["message"]["chat"]["id"];
+    var name=req.body["message"]["chat"]["first_name"];
+    var text=req.body["message"]["text"];
 
     getRandomMemeAsBase64()
     .then(response=>{
-            let base64 = response;
-            let url = website+"/sendphoto?chat_id="+chatId+"&photo="+base64;
+            var base64 = response;
+            var url = website+"/sendphoto?chat_id="+chatId+"&photo="+base64;
             sendMemeToTelegram(url)
             .then(response=>{
                 res.send();
@@ -59,7 +59,7 @@ function getRandomMemeAsBase64(){
         getRandomFromFrinkiac()
         .then(response=>{
             const buff = Buffer.from(response.caption, 'utf-8');
-            let captionBase64 = buff.toString('base64');
+            var captionBase64 = buff.toString('base64');
             getBase64MemeFromFrinkiac(response.episode, response.timeStamp, captionBase64)
             .then(meme=>{
                 resolve(meme);
@@ -76,15 +76,15 @@ function getRandomMemeAsBase64(){
 
 function getRandomFromFrinkiac(){
     return new Promise( (resolve, reject) =>{
-        let url = 'https://frinkiac.com/api/random';
+        var url = 'https://frinkiac.com/api/random';
         axios.get(url)
         .then(response => {
-            let longCaption = "";
+            var longCaption = "";
             response.data.Subtitles.forEach(element => {
                 longCaption+=" ".concat(element.Content)
             });
-            let episode = response.data.Frame.Episode;
-            let timeStamp = response.data.Frame.Timestamp;
+            var episode = response.data.Frame.Episode;
+            var timeStamp = response.data.Frame.Timestamp;
             resolve({episode: episode, timeStamp: timeStamp, caption: longCaption});
         })
         .catch(error => {
@@ -95,14 +95,14 @@ function getRandomFromFrinkiac(){
 
 function getBase64MemeFromFrinkiac(episode,timestamp,captionInBase64){
     return new Promise( (resolve, reject) =>{
-        let url = 'https://frinkiac.com/meme/'+episode+'/'+timestamp+'.jpg?b64lines='+captionInBase64;
+        var url = 'https://frinkiac.com/meme/'+episode+'/'+timestamp+'.jpg?b64lines='+captionInBase64;
         axios.get(url, {
             responseType: 'arraybuffer'
         })
         .then(response => {
-            let mimetype="image/jpeg";
-            let base64 = Buffer.from(response.data, 'binary').toString('base64');
-            let imageBase64 = "data:"+mimetype+";base64,"+base64;
+            var mimetype="image/jpeg";
+            var base64 = Buffer.from(response.data, 'binary').toString('base64');
+            var imageBase64 = "data:"+mimetype+";base64,"+base64;
             resolve(base64);
         })
         .catch(error => {
@@ -111,6 +111,8 @@ function getBase64MemeFromFrinkiac(episode,timestamp,captionInBase64){
     });
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
