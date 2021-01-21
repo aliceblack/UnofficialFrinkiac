386ce2546e898bff5d4e005fc4a08cdcad92f92d
Alice Sasso <alice.sasso@wintech.it>
2021-01-21 12:51:18 +0100

    Carriage return at 30 char

--- index.js
+++ index.js
@@ -83,8 +83,9 @@ function getRandomFromFrinkiac(){
         .then(response => {
             var longCaption = "";
             response.data.Subtitles.forEach(element => {
-                longCaption=longCaption.concat(element.Content).concat("\n")
+                longCaption=longCaption.concat(element.Content).concat(" ")
             });
+            longCaption = longCaption.replace(/.{30}/g, "$0\n")
             var episode = response.data.Frame.Episode;
             var timeStamp = response.data.Frame.Timestamp;
             resolve({episode: episode, timeStamp: timeStamp, caption: longCaption});
